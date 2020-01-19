05b89341d60e04e0b17c46f2ce8af57a393cbea9
RubbaBoy <adamyarris@gmail.com>
2020-01-19 08:11:47 -0500

    Google sign-in working, committing as a checkpoint. Definitely going to be moved shortly

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,9 +1,13 @@
+import 'dart:html';
+import 'dart:js';
+
 import 'package:HolySheetWeb/src/file_list/file_list_component.dart';
 import 'package:HolySheetWeb/src/file_service.dart';
 import 'package:HolySheetWeb/src/routes.dart';
 import 'package:HolySheetWeb/src/settings_service.dart';
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
+import 'package:googleapis_auth/auth_browser.dart';
 
 @Component(
   selector: 'my-app',
@@ -26,7 +30,16 @@ class AppComponent implements OnInit {
 
   @override
   Future<void> ngOnInit() async {
-    print('Authenticating here or whatever');
+    var id = ClientId('916425013479-6jdls4crv26mhurj43eakbs72f5e1m8t.apps.googleusercontent.com', null);
+    var scopes = ['profile', 'email', 'https://www.googleapis.com/auth/drive'];
+
+    await createImplicitBrowserFlow(id, scopes).then((BrowserOAuth2Flow flow) {
+      flow.runHybridFlow(force: true).then((hybrid) {
+        final code = hybrid.authorizationCode;
+        print('code = $code');
+        HttpRequest.getString('http://localhost:8090/callback?code=$code');
+      });
+    });
   }
 
 }
