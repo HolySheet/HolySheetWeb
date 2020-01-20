d515b79fc860d6c4fa15ecaf534b9f4521276eb7
RubbaBoy <adamyarris@gmail.com>
2020-01-19 20:17:18 -0500

    OAuth kinda works? (Still a huge WIP)

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,5 +1,5 @@
+import 'dart:async';
 import 'dart:html';
-import 'dart:js';
 
 import 'package:HolySheetWeb/src/file_list/file_list_component.dart';
 import 'package:HolySheetWeb/src/file_service.dart';
@@ -23,6 +23,15 @@ import 'package:googleapis_auth/auth_browser.dart';
   exports: [Routes, RoutePaths],
 )
 class AppComponent implements OnInit {
+  final id = ClientId(
+      '916425013479-6jdls4crv26mhurj43eakbs72f5e1m8t.apps.googleusercontent.com',
+      null);
+  final scopes = [
+    'openid',
+    'profile',
+    'email',
+    'https://www.googleapis.com/auth/drive'
+  ];
 
   final FileService fileService;
 
@@ -30,16 +39,42 @@ class AppComponent implements OnInit {
 
   @override
   Future<void> ngOnInit() async {
-    var id = ClientId('916425013479-6jdls4crv26mhurj43eakbs72f5e1m8t.apps.googleusercontent.com', null);
-    var scopes = ['profile', 'email', 'https://www.googleapis.com/auth/drive'];
+    await getIdToken().then((idToken) async {
+      print('idToken = $idToken');
 
-    await createImplicitBrowserFlow(id, scopes).then((BrowserOAuth2Flow flow) {
-      flow.runHybridFlow(force: true).then((hybrid) {
-        final code = hybrid.authorizationCode;
-        print('code = $code');
-        HttpRequest.getString('http://localhost:8090/callback?code=$code');
+      await HttpRequest.postFormData('http://localhost:8090/check', {
+        'Authorization': idToken,
       });
     });
   }
 
+  /// Logs in a user
+  Future<void> loginUser() async =>
+      await createImplicitBrowserFlow(id, scopes).then((BrowserOAuth2Flow flow) {
+        flow.runHybridFlow(immediate: false, force: true).then((hybrid) {
+          final code = hybrid.authorizationCode;
+
+          HttpRequest.getString('http://localhost:8090/callback?code=$code');
+        });
+      });
+
+  /// Gets the token. If not null, it should then be sent to :api:/check to
+  /// verify it has been stored in the system. If not, the user is not
+  /// considered authenticated.
+  Future<String> getIdToken() async =>
+      await createImplicitBrowserFlow(id, scopes)
+          .then((BrowserOAuth2Flow flow) {
+        try {
+          return flow.obtainAccessCredentialsViaUserConsent(
+              immediate: true,
+              force: true,
+              responseTypes: [
+                ResponseType.idToken,
+                ResponseType.token
+              ]).then((hybrid) => hybrid.idToken);
+        } catch (e) {
+          print('An error happened, user not signed in');
+          return null;
+        }
+      });
 }
